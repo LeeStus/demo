a798644a76e10551420279bbb42297199632f2d2
leewenbin <1007753664@qq.com>
2019-11-14 18:43:44 +0800

    all

--- demo-controller/src/main/java/com/day/demo/controller/testController.java
+++ demo-controller/src/main/java/com/day/demo/controller/testController.java
@@ -1,5 +1,7 @@
 package com.day.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
 
+    Logger log = LoggerFactory.getLogger(testController.class);
+
     @PostMapping("/test")
     public String test(){
 
+        log.info("日志记录");
         return "Dispatch servlet";
     }
 }
