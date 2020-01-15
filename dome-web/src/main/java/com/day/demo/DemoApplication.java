4ca654a783efaa7544699575817673a36c28f163
leewenbin <1007753664@qq.com>
2020-01-15 14:25:38 +0800

    更新

--- dome-web/src/main/java/com/day/demo/DemoApplication.java
+++ dome-web/src/main/java/com/day/demo/DemoApplication.java
@@ -1,8 +1,8 @@
 package com.day.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Demo class
  *
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019/12/10
  */
 @SpringBootApplication
-@MapperScan("com.day.demo.mybatis.mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
